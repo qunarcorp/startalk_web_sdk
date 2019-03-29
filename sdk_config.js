811e782c75b17c65e74709c722df389802151dd2
yuelin.liu <yuelin.liu@qunar.com>
2019-03-29 18:01:49 +0800

    config

--- sdk_config.js
+++ sdk_config.js
@@ -3,11 +3,11 @@
  * 其中 pub_key_fullkey的值需要生成，详见node项目readme.md
  * */
 module.exports = {
-    "domain":"qtalk.test.org",
-    "pub_key_fullkey": "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCy2VXDAlCZlj7gPHvC/vwvbpTN\n/GyW0tmNCqh0UPitdTTGZk3UcLqu9lWMGPViL/5lhboiSogsDxJLHdwo91DDBjTX\n1HbuyuOhvsvayV7Yc8t+ajFW/8RwlvhGSzVplthoU+md9kGeZ8t73VWWZUEB0iyW\nx7Y/RjUwTdnOlNXDzQIDAQAB\n-----END PUBLIC KEY-----",
-    "fileurl":"http://100.80.128.247:8080",
-    "javaurl":"http://100.80.128.247:8080/package",
-    "httpurl":"http://100.80.128.247:8080/newapi",
-    "apiurl":"http://100.80.128.247:8080/api",
-    "searchurl":"http://100.80.128.247:8080/search/search.py"
+    "domain":"",
+    "pub_key_fullkey": "",
+    "fileurl":"",
+    "javaurl":"",
+    "httpurl":"",
+    "apiurl":"",
+    "searchurl":""
 }
